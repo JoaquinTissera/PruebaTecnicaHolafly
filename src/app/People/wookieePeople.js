31e9524fde21b81a672651ff7bb16754c3f86923
Joaquin Tissera <81702@sistemas.frc.utn.edu.ar>
2023-09-25 19:39:25 -0300

    feat(getPeopleSWAIPInWookiee): agregamos la funcionalidad adecuada para que podamos utilizar el formato wookie

--- src/app/People/wookieePeople.js
+++ src/app/People/wookieePeople.js
@@ -1,7 +1,29 @@
 const AbstractPeople = require('./abstractPeople')
+const { genericRequest } = require('../swapiFunctions/')
 
 class WookieePeople extends AbstractPeople {
-    
+    constructor(id){
+        super()
+        this.id = id
+    }
+
+    async init(){
+        const people = await this.getPeopleSWAIPInWookiee();
+
+        this.name = people.name;
+        this.mass = people.mass;
+        this.height = people.height;
+        this.homeworld_name = people.homeworld_name;
+        this.homeworld_id = people.homeworld_id;
+    }
+
+    async getPeopleSWAIPInWookiee(){
+        const { whrascwo: name, scracc: mass, acwoahrracao: height, acooscwoohoorcanwa: homeworld} = await genericRequest(`https://swapi.dev/api/people/${this.id}?format=wookiee`, "GET");
+        const homeworld_id = homeworld.split("/")[5];
+        const { whrascwo: homeworld_name} = await genericRequest(`https://swapi.dev/api/planets/${homeworld_id}?format=wookiee`, "GET");
+        return { name, mass, height, homeworld_name, homeworld_id: homeworld_id};
+    }
+
 }
 
 module.exports = WookieePeople;
