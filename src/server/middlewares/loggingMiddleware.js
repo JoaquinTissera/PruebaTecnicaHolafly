89d631add3bbe5425c79c488909da2fce0ef81c2
Joaquin Tissera <81702@sistemas.frc.utn.edu.ar>
2023-09-25 22:47:06 -0300

    feat(getLogs): una llamada a la base de datos para persister las llamadas antereiores al logout de la misma

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,9 +1,16 @@
+const db = require("../../app/db")
+
 const loggingMiddleware = (db) =>
     (req, res, next) => {
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
         const headers = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
         // Persist this info on DB
+        db.logging.create({
+            action: originalUrl,
+            header: JSON.stringify(headers),
+            ip: ip
+        })
         next();
     }
 
